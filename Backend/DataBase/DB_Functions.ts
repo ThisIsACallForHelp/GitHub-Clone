78e35bad49d9f2a8298ed7e33572519cc081c1a4
ac8274 <ariechera@gmail.com>
2026-08-11 00:40:53 +0300

    added some functions to db

--- Backend/DataBase/DB_Functions.ts
+++ Backend/DataBase/DB_Functions.ts
@@ -1,6 +1,23 @@
+import { FileChange } from "../../shared/snapshot.types.ts"
+import {sql} from "kysely"
 import db from "./DB_init.ts"
 
-
 export const dbFuncs = {
-    
+    getSnapshotByID: async (id: string) => {
+        return await db.selectFrom("snapshots").where("snapshotID" , '=' , id).selectAll().execute()
+    },
+    getSnapshotsBiggerTime: async (time: number) => {
+        return await db.selectFrom("snapshots").where("timestamp" , '<' , time).selectAll().execute()
+    },
+    getAllSnapshots: async () => {
+        return await db.selectFrom("snapshots").selectAll().execute()
+    },
+    addFileChangeById: async (id: string, filechange: FileChange) => {
+        return await db.updateTable("snapshots")
+        .set({
+            changes: sql`array_append(changes, ${filechange}::FileChange)`
+        })
+        .where("snapshotID" , '=' , id)
+        .execute()
+    }
 }
